eb33875c857712972b918855f2319ac69f535286
aronhr <aronhr98@gmail.com>
2015-03-17 23:53:21 +0000

    reupload
    reupload

---
+++ index.php
@@ -0,0 +1,97 @@
+﻿<?php 
+include 'include/config.php'; 
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+	  <meta charset="utf-8"/>
+	  <title><?php echo($all['titill']); ?></title>
+
+	  <link rel="stylesheet" type="text/css" href="http://meyerweb.com/eric/tools/css/reset/reset.css">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap-theme.min.css">
+	  <link rel="stylesheet" href="css/styles.css" type="text/css">
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+
+<body>
+<div id="preloader">
+	<div id="status"></div>
+</div>
+<div>
+	<header>
+		<!-- <div class="title">
+			<h1>CS:GO</h1>
+			<p>Counter Strike : Global Offensive</p>
+		</div> -->
+	</header>
+	<body>
+	
+		<div class="container marketing">
+		<div class="bil"></div>
+		<!-- Lína á milli div'a -->
+<!-- 		<hr class="featurette-divider">
+ -->		<!-- / Lína á milli div'a -->
+	
+      <!-- 3 Columns -->
+    	
+	      <div class="row center">
+	        <div class="col-lg-4">
+	          <img class="img-circle center" src="https://scontent-lhr.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/1526624_10152142696322904_1716991672_n.jpg?oh=e96f8e11d462829f29d7663cdd4993ba&oe=557006DC" width="140" height="140">
+	          <h2>Heading</h2>
+	          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	      						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	      						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	      						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	      						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	      						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+	        </div><!-- /.col-lg-4 -->
+	        <div class="col-lg-4">
+	          <img class="img-circle center" src="https://scontent-lhr.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/1526624_10152142696322904_1716991672_n.jpg?oh=e96f8e11d462829f29d7663cdd4993ba&oe=557006DC" width="140" height="140">
+	          <h2>Heading</h2>
+	          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	      						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	      						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	      						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	      						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	      						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+	        </div><!-- /.col-lg-4 -->
+	        <div class="col-lg-4">
+	          <img class="img-circle center" src="https://scontent-lhr.xx.fbcdn.net/hphotos-xpa1/v/t1.0-9/1526624_10152142696322904_1716991672_n.jpg?oh=e96f8e11d462829f29d7663cdd4993ba&oe=557006DC" width="140" height="140">
+	          <h2>Heading</h2>
+	          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	      						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	      						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	      						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	      						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	      						proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	          <p><a class="btn btn-default" href="#" role="button">View details &raquo;</a></p>
+	        </div><!-- /.col-lg-4 -->
+	      </div><!-- /.row -->
+	      <!-- Lína á milli div'a -->
+		<div class="bil"></div>
+		<!-- / Lína á milli div'a -->
+	      </div>
+
+	     
+
+	     <div class="main-body">
+	     	
+	     </div>
+
+      <footer>
+      	
+
+      </footer>
+      
+	</body>
+</div>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6/jquery.min.js"></script>
+<script type="text/javascript" src="js/preloader.js"></script>
+</body>
+</html>
