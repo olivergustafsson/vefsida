12c38b11ea59b0272c25fc1d5532d05975defef0
Aron <aronhr98@gmail.com>
2015-03-24 15:08:18 +0000

    Lagaði order.php

--- order.php
+++ order.php
@@ -25,22 +25,22 @@ include 'include/config.php';
 			<form action="email.php" method="post"><div style="margin:0;padding:0;display:inline">    
 			<div class="row contact-form mt80">
        		 <div class="col-md-6">
-       		   <label>First Name: </label>
-       		   <input type="text" name="firstname" class="form-control"/>
+       		   <label>First Name:</label>
+       		   <input type="text" name="firstname" class="form-control" required patttern="[a-zA-Z0-9]+"/>
        		   <label>Last Name: </label>
-       		   <input type="text" name="lastname" class="form-control"/>
+       		   <input type="text" name="lastname" class="form-control" required patttern="[a-zA-Z0-9]+"/>
        		   <label>Email:</label>
-       		   <input type="email" name="email" class="form-control"/>
+       		   <input type="email" name="email" class="form-control" required/>
        		 </div>
        		 <div class="col-md-6">
        		 	<label>Phone:</label>
-       		   	<input type="phone" name="phone number" class="form-control"/>  
+       		   	<input type="phone" name="phone number" class="form-control" required />  
        		 	<label>Color:</label>
-       		   	<input type="text" name="car color" class="form-control"/>  
+       		   	<input type="text" name="car color" class="form-control" required/>  
        		   	<label>Type:</label>
-       		   	<input type="text" name="BMW type" class="form-control"/> 
+       		   	<input type="text" name="BMW type" class="form-control" required/> 
        		   <div class="bil"></div>
-       		   <input type="submit" value="Submit" class="btn btn-danger btn-lg"/>      
+       		   <input type="submit" value="Submit" class="btn btn-danger btn-lg" required/>      
        		 </div>
 
       			</div>
